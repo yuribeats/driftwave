866bfbf30c179d9907534edee6d6d0496c241424
Yuri Rybak <w.yuri.rybak@gmail.com>
2026-03-25 00:22:52 -0400

    Add Google Analytics G-Y5DHE8MYGN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y5DHE8MYGN" />
+        <script dangerouslySetInnerHTML={{ __html: "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-Y5DHE8MYGN');" }} />
+      </head>
       <body className={`${ibmPlexMono.variable} ${audiowide.variable} ${vt323.variable} antialiased`}>
         <AudioWarmup />
         {children}
